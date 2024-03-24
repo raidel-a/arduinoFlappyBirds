449d728c33d140530594b65a1d9a09b5aa8da15b
raidel-a <raihven@protonmail.com>
2024-03-24 09:43:12 -0400

    final push

--- arduinoFlappyBirds.ino
+++ arduinoFlappyBirds.ino
@@ -1,275 +1,301 @@
 #include <Wire.h>
-#include <LiquidCrystal.h>
-// #include <LiquidCrystal_I2C.h>
+#include <rgb_lcd.h>
+#include "pitches.h"
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // Initialize the LCD display
+rgb_lcd lcd;
 
-#define buttonPin 8 // Define the button pin, tells the arduino what pin the button is connected to
+#define buttonPin 4
+#define BUZZER_PIN 8
 
-// The pipe object is defined as an array of bytes.
-// It represents a custom character that will be displayed on an LCD screen.
-// The pipe array is initialized with 8 elements, each representing a row of the character.
-// Each row is defined using a binary number that represents the pattern of pixels in that row.
-// The binary numbers are written in the form of 0bXXXXXXXX, where each X represents a pixel.
-// For example, the first row of the pipe character is defined as 0b11111, which means that all 5 pixels in that row are turned on.
-// The second row is also 0b11111, indicating that all 5 pixels in that row are turned on as well.
-// The following rows have a similar pattern, forming the shape of a pipe.
-// By defining the pipe character in this way, it can be easily displayed on the LCD screen by sending the corresponding byte values to the LCD library.
-// The LCD library will interpret these byte values and display the custom character accordingly.
+bool hasPlayedDeadChime = false;
+void startupChime() {
+  tone(BUZZER_PIN, 1000); // Play a 1kHz tone
+  delay(100); // Wait for 100ms
+  noTone(BUZZER_PIN); // Stop the tone
+}
+void coinChime() {
+  tone(BUZZER_PIN, NOTE_B5);
+  delay(200);
+  noTone(BUZZER_PIN);
+  analogWrite(BUZZER_PIN, NOTE_E6);
+  delay(400);
+  noTone(BUZZER_PIN);
+}
+void collisionChime() {
+  tone(BUZZER_PIN, NOTE_A4);
+  delay(300);
+  noTone(BUZZER_PIN);
+  tone(BUZZER_PIN, NOTE_A4);
+  delay(200);
+  noTone(BUZZER_PIN);
+}
+void deadChime() {
+  if (!hasPlayedDeadChime){
+  tone(BUZZER_PIN, NOTE_B5);
+  delay(300);
+  noTone(BUZZER_PIN);
+  tone(BUZZER_PIN, NOTE_F5);
+  delay(300);
+  noTone(BUZZER_PIN);
+  tone(BUZZER_PIN, NOTE_F5);
+  delay(300);
+  noTone(BUZZER_PIN);
+  tone(BUZZER_PIN, NOTE_E5);
+  delay(300);
+  noTone(BUZZER_PIN);
+  tone(BUZZER_PIN, NOTE_D4);
+  delay(300);
+  noTone(BUZZER_PIN);
+  tone(BUZZER_PIN, NOTE_C4);
+  delay(300);
+  noTone(BUZZER_PIN);
+  tone(BUZZER_PIN, NOTE_E4);
+  delay(300);
+  noTone(BUZZER_PIN);
+  tone(BUZZER_PIN, NOTE_E4);
+  delay(300);
+  noTone(BUZZER_PIN);
+  tone(BUZZER_PIN, NOTE_C4);
+  delay(300);
+  noTone(BUZZER_PIN);
+  hasPlayedDeadChime = true;
+}}
 
-byte pipe[8] = { // Define the pipe obstacle
-    0b00000,
-    0b11111,
-    0b11111,
-    0b01110,
-    0b01110,
-    0b01110,
-    0b01110,
-    0b01110};
+byte pipe[8] = {
+  0b00000,
+  0b11111,
+  0b11111,
+  0b01110,
+  0b01110,
+  0b01110,
+  0b01110,
+  0b01110
+};
 
-byte pipeInverted[8] = {
-    0b01110,
-    0b01110,
-    0b01110,
-    0b01110,
-    0b01110,
-    0b11111,
-    0b11111,
-    0b00000};
+byte pipeTop[8] = {
+  0b01110,
+  0b01110,
+  0b01110,
+  0b01110,
+  0b01110,
+  0b11111,
+  0b11111,
+  0b00000
+};
 
-byte bird[8] = { // Define the bird character
-    0b00000,
-    0b00110,
-    0b10111,
-    0b11110,
-    0b11110,
-    0b01100,
-    0b00000,
-    0b00000};
+byte bird[8] = {
+  0b00000,
+  0b00110,
+  0b10111,
+  0b11110,
+  0b11110,
+  0b01100,
+  0b00000,
+  0b00000
+};
 
 byte birdAnim[8] = {
-    0b00000,
-    0b00000,
-    0b00110,
-    0b10111,
-    0b11110,
-    0b11110,
-    0b01100,
-    0b01000};
+  0b00000,
+  0b00000,
+  0b00110,
+  0b10111,
+  0b11110,
+  0b11110,
+  0b01100,
+  0b01000
+};
 
-byte hit[8] = { // Define the hit marker
-    0b10001,
-    0b11011,
-    0b01010,
-    0b00100,
-    0b00100,
-    0b01010,
-    0b11011,
-    0b10001};
+byte hit[8] = {
+  0b10001,
+  0b11011,
+  0b01010,
+  0b00100,
+  0b00100,
+  0b01010,
+  0b11011,
+  0b10001
+};
 
-byte heart[8] = { // Define the heart symbol
-    0b00000,
-    0b01010,
-    0b11111,
-    0b11111,
-    0b11111,
-    0b01110,
-    0b00100,
-    0b00100};
+byte heart[8] = {
+  0b00000,
+  0b01010,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b01110,
+  0b00100,
+  0b00100
+};
 
-byte coin[8] = { // Define the coin symbol
-    0b00000,
-    0b01110,
-    0b11011,
-    0b11111,
-    0b11011,
-    0b11011,
-    0b01110,
-    0b00000};
+byte coin[8] = {
+  0b00000,
+  0b01110,
+  0b11011,
+  0b11111,
+  0b11011,
+  0b11011,
+  0b01110,
+  0b00000
+};
 
-int pipes[16];       // Array to store the pipe positions
-int coins[16] = {0}; // Array to store the coin positions
-int birdY = 2;       // Initial bird position on the Y-axis
-int birdX = 7;       // Initial bird position on the X-axis
-int score = 0;       // Current score
-int speedlvl = 300; // Speed of pipes
-int life = 3;        // Number of lives
+int pipes[16];
+int topPipes[16] = { 0 };
+int coins[16] = { 0 };
+int birdY = 2;
+int birdX = 7;
+int score = 0;
+int speed = 400;
+int life = 3;
 
-void setup()
-{
-  // lcd.backlight();  only needed for I2C use
-  Serial.begin(9600); // Start the serial communication, used for debugging,
-                      // not needed for the game,
-                      // but can be useful to see the score in the serial monitor
-
-  lcd.begin(16, 2); // Initialize the LCD display with 16 columns and 2 rows
-
-  lcd.createChar(0, pipe);  // Create the pipe character
-  lcd.createChar(1, bird);  // Create the bird character
-  lcd.createChar(2, hit);   // Create the hit character
-  lcd.createChar(3, heart); // Create the heart character
+void setup() {
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.createChar(0, pipe);
+  lcd.createChar(1, bird);
+  lcd.createChar(2, hit);
+  lcd.createChar(3, heart);
   lcd.createChar(4, birdAnim);
-  lcd.createChar(6, coin); // Create the coin character
-
-  pinMode(buttonPin, INPUT_PULLUP); // Set the button pin as an input and enable the internal pull-up resistor
+  lcd.createChar(5, pipeTop);
+  lcd.createChar(6, coin);
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
-void birdfly()
-{
-  int buttonValue = digitalRead(buttonPin); // Read the button value
+void birdfly() {
+  int buttonValue = digitalRead(buttonPin);
   int charId = 0;
 
-  if (buttonValue == LOW)
-  {             // If the button is pressed
-    birdY = 0;  // Move the bird up
-    charId = 1; // Set the character ID to display the bird
-  }
-  else
-  {
-    birdY = 1;  // Move the bird down
-    charId = 1; // Set the character ID to display the bird
-    // if (pipes[birdX] == 1) {  // If the bird hits a pipe, does this by checking if the pipe is at the same position as the bird
-    //   // charId = 2;             // Set the character ID to display the hit character
-    //   lcd.setCursor(birdX, birdY);
-    //   lcd.write(2); // Display the hit marker
-    //   life--;                 // Decrease the number of lives
-    //   score--;                // Decrease the score
-    // }
+  if (buttonValue == HIGH) {
+    birdY = 0;
+    charId = 1;
+  } else {
+    birdY = 1;
+    charId = 1;
   }
 
-  // lcd.setCursor(birdX, birdY);  // Set the cursor position on the LCD display
-  // lcd.write((byte)charId);      // Display the character
-  if (birdY == 0)
-  {
+  if (birdY == 0) {
     lcd.setCursor(birdX, birdY);
-    lcd.write(1); // Display the bird
-  }
-  else
-  {
+    lcd.write(1);
+  } else {
     lcd.setCursor(birdX, birdY);
-    lcd.print(' '); // Clear the previous bird position
+    lcd.print(' ');
     birdY = 1;
     lcd.setCursor(birdX, birdY);
-    lcd.write(1); // Display the bird
+    lcd.write(1);
   }
 }
 
-void checkCollision()
-{
-  if (pipes[birdX] == 1 
-  // && birdY != 0
-  )
-  { // If the bird hits a pipe and it is not at the top position
+void checkCollision() {
+  if ((pipes[birdX] == 1 && birdY != 0) || (topPipes[birdX] == 1 && birdY == 0)) {
     lcd.setCursor(birdX, birdY);
-    lcd.write(2); // Display the hit marker
-    life--;       // Decrease the number of lives
-    score -= 5;   // Decrease the score by 5
-  }
-  else if (coins[birdX] == 1)
-  { // If the bird hits a coin
+    lcd.write(2);
+    life--;
+    score -= 5;
+    collisionChime();
+  } else if (coins[birdX] == 1 && birdY != 1) {
     lcd.setCursor(birdX, birdY);
-    lcd.write(1); // Display the bird
-    coins[birdX] = 0; // Remove the coin
-    score += 2; // Increase the score by 2
+    lcd.write(1);
+    coins[birdX] = 0;
+    score += 5;
+    coinChime();
   }
 }
 
-void movePipes()
-{
-  // Move the pipes and coins to the left
-  for (int i = 1; i < 16; i++)
-  {
-    pipes[i - 1] = pipes[i]; // Move the pipes to the left
-    coins[i - 1] = coins[i]; // Move the coins to the left
+void movePipes() {
+  for (int i = 1; i < 16; i++) {
+    pipes[i - 1] = pipes[i];
+    topPipes[i - 1] = topPipes[i];
+    coins[i - 1] = coins[i];
   }
 
-  // Generate new pipes and possibly a coin at the right side of the screen
-  if (random(10) < 2) { // 20% chance to generate a coin
-    coins[15] = 1; // Coin
-    pipes[15] = 0; // No pipe
-  } else {
-    pipes[15] = random(0, 2); // Pipe or gap
-    coins[15] = 0; // No coin
-  }
-
-  // Draw the pipes and coins
-  for (int i = 0; i < 16; i++)
-  {
-    lcd.setCursor(i, 1);
-    if (pipes[i] != 0) {
-      lcd.write((byte)0); // Display the pipe
-    } else if (coins[i] == 1) {
-      lcd.write((byte)6); // Display the coin
+  if (pipes[14] == 0 && topPipes[14] == 0) {
+    if (random(2) == 0) {
+      pipes[15] = random(0, 2);
+      topPipes[15] = 0;
     } else {
-      lcd.write(' '); // No pipe or coin
+      topPipes[15] = random(0, 2);
+      pipes[15] = 0;
+    }
+    if (pipes[15] == 0 && topPipes[15] == 0) {
+      coins[15] = random(0, 2);
+    } else {
+      coins[15] = 0;
+    }
+  } else {
+    pipes[15] = 0;
+    topPipes[15] = 0;
+    coins[15] = 0;
+  }
+
+  for (int i = 0; i < 16; i++) {
+    lcd.setCursor(i, 0);
+    if (topPipes[i] == 1) {
+      lcd.write((byte)5);
+    } else if (coins[i] == 1) {
+      lcd.write((byte)6);
+    } else {
+      lcd.write(' ');
+    }
+
+    lcd.setCursor(i, 1);
+    if (pipes[i] == 1) {
+      lcd.write((byte)0);
+    } else {
+      lcd.write(' ');
     }
   }
 }
 
-void showLives()
-{
+void showLives() {
   int i;
-  for (i = 0; i < life; i++)
-  {
-    lcd.setCursor(i, 0); // Set the cursor position on the LCD display
-    lcd.write((byte)3);  // Display the heart character
+  for (i = 0; i < life; i++) {
+    lcd.setCursor(i, 0);
+    lcd.write((byte)3);
   }
 }
 
-void showScore()
-{
-  lcd.setCursor(3, 0); // Set the cursor position on the LCD display
-  lcd.print(score);    // Display the score
+void showScore() {
+  lcd.setCursor(3, 0);
+  lcd.print(score);
 }
 
 bool gameStarted = false;
 
-void loop()
-{
-  lcd.clear(); // Clear the LCD display
+void loop() {
+  lcd.clear();
 
-  if (!gameStarted)
-  {
-    // Display the start screen
+  if (!gameStarted) {
+    lcd.setCursor(1, 0);
+    lcd.write(4);
+    delay(300);
+
+    lcd.setCursor(3, 0);
+    lcd.print("Flappy Bird");
+    lcd.setCursor(0, 1);
+    lcd.print("Press the button");
 
     lcd.setCursor(1, 0);
-    lcd.write(4); // Display the bird
-    delay(300);   // in milliseconds
-
-    lcd.setCursor(3, 0);           // Set the cursor position on the LCD display
-    lcd.print("Flappy Bird");      // Display "Flappy Bird"
-    lcd.setCursor(0, 1);           // Set the cursor position on the LCD display
-    lcd.print("Press the button"); // Display "Press the button"
-
-    lcd.setCursor(1, 0);
-    lcd.write(1); // Display the bird animation
+    lcd.write(1);
     delay(100);
-
-    // Check if the button is pressed
-    if (digitalRead(buttonPin) == LOW)
-    {
-      // If the button is pressed, start the game
+    startupChime();
+    if (digitalRead(buttonPin) == HIGH) {
       gameStarted = true;
     }
-  }
-  else if (life > 0)
-  { // If there are lives remaining
+  } else if (life > 0) {
     movePipes();
     birdfly();
     showLives();
     showScore();
     checkCollision();
     score++;
-    speedlvl--; // Decrease the speed level
-  }
-  else
-  {
-    lcd.setCursor(3, 0); // Set the cursor position on the LCD display
+    speed--;
+  } else {
+    lcd.setCursor(3, 0);
     lcd.print("GAME OVER");
-    lcd.setCursor(3, 1); // Set the cursor position on the LCD display
+    lcd.setCursor(3, 1);
     lcd.print("Score: ");
-    lcd.print(score); // Display the final score
+    lcd.print(score);
+    deadChime();
   }
 
-  delay(speedlvl); // Delay for the specified speed level
+  delay(speed);
 }
